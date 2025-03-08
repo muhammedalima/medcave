67c9d4f83d86fe77c4401314b43a06579a6f4cc5
muhammedali8157ma <muhammedali8157ma@gmail.com>
2025-03-08 10:17:12 +0000

    added navgation bar logic

--- lib/Users/Mobilescreens/bottom_navigation_bar/bottom_navigation_bar.dart
+++ lib/Users/Mobilescreens/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -7,6 +7,8 @@ import 'package:medcave/Users/Mobilescreens/features/ambulance_tracking/presenta
 import 'package:medcave/Users/Mobilescreens/features/home_screen/presentation/pages/home_page.dart';
 import 'package:medcave/Users/Mobilescreens/features/hospital_features/presentation/pages/hospital_page.dart';
 import 'package:medcave/Users/Mobilescreens/features/personal_profile/presentation/pages/profile_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CustomNavigationBar extends StatefulWidget {
   const CustomNavigationBar({super.key});
@@ -17,15 +19,127 @@ class CustomNavigationBar extends StatefulWidget {
 
 class _CustomNavigationBarState extends State<CustomNavigationBar> {
   int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    HomePage(),
-    HospitalPage(),
-    AmbulanceDriverPage(),
-    ProfilePage(),
-  ];
+  bool _isAmbulanceDriver = false;
+  bool _isLoading = true;
+  
+  late List<Widget> _widgetOptions;
+  late List<GButton> _navButtons;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkUserRole();
+  }
+
+  Future<void> _checkUserRole() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      try {
+        final userData = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .get();
+        
+        if (userData.exists) {
+          final bool isAmbulanceDriver = userData.data()?['isAmbulanceDriver'] ?? false;
+          
+          setState(() {
+            _isAmbulanceDriver = isAmbulanceDriver;
+            _isLoading = false;
+            _initializeWidgets();
+          });
+        } else {
+          setState(() {
+            _isAmbulanceDriver = false;
+            _isLoading = false;
+            _initializeWidgets();
+          });
+        }
+      } catch (e) {
+        print('Error fetching user role: $e');
+        setState(() {
+          _isAmbulanceDriver = false;
+          _isLoading = false;
+          _initializeWidgets();
+        });
+      }
+    } else {
+      setState(() {
+        _isAmbulanceDriver = false;
+        _isLoading = false;
+        _initializeWidgets();
+      });
+    }
+  }
+
+  void _initializeWidgets() {
+    // Initialize widget options based on user role
+    if (_isAmbulanceDriver) {
+      _widgetOptions = const <Widget>[
+        HomePage(),
+        HospitalPage(),
+        AmbulanceDriverPage(),
+        ProfilePage(),
+      ];
+      
+      _navButtons = const [
+        GButton(
+          icon: Octicons.home,
+          text: 'Home',
+        ),
+        GButton(
+          icon: FontAwesome.plus_squared_alt,
+          text: 'Hospital',
+        ),
+        GButton(
+          icon: FontAwesome.ambulance,
+          text: 'Ambulance',
+        ),
+        GButton(
+          icon: Icons.person_2_outlined,
+          text: 'Profile',
+        ),
+      ];
+    } else {
+      // Non-ambulance driver - hide ambulance tab
+      _widgetOptions = const <Widget>[
+        HomePage(),
+        HospitalPage(),
+        ProfilePage(),
+      ];
+      
+      _navButtons = const [
+        GButton(
+          icon: Octicons.home,
+          text: 'Home',
+        ),
+        GButton(
+          icon: FontAwesome.plus_squared_alt,
+          text: 'Hospital',
+        ),
+        GButton(
+          icon: Icons.person_2_outlined,
+          text: 'Profile',
+        ),
+      ];
+    }
+    
+    // Reset selected index if needed
+    if (_selectedIndex >= _widgetOptions.length) {
+      _selectedIndex = 0;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+    
     return Scaffold(
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
@@ -43,24 +157,7 @@ class _CustomNavigationBarState extends State<CustomNavigationBar> {
             padding: const EdgeInsets.all(16),
             duration: const Duration(milliseconds: 1000),
             iconSize: 25,
-            tabs: const [
-              GButton(
-                icon: Octicons.home,
-                text: 'Home',
-              ),
-              GButton(
-                icon: FontAwesome.plus_squared_alt,
-                text: 'Hospital',
-              ),
-              GButton(
-                icon: FontAwesome.ambulance,
-                text: 'Ambulance',
-              ),
-              GButton(
-                icon: Icons.person_2_outlined,
-                text: 'Profile',
-              ),
-            ],
+            tabs: _navButtons,
             selectedIndex: _selectedIndex,
             onTabChange: (index) {
               setState(() {
